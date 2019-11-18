83deaf9638ad86d775787e433082aaf2b4ce4df7
manu200921 <manu200921@gmail.com>
2019-11-18 09:29:19 -0600

    Update Business Recommendation

--- Code/Shiny/server.R
+++ Code/Shiny/server.R
@@ -19,18 +19,20 @@ server <- function(input, output) {
     review[city == input$cityname]
   })
   
+  
   output$yelp  <- renderLeaflet({ 
     
-    #show description when you click the point on the map
+    # show description when you click the point on the map
     description <- paste("Name: ", business_city()$name, "<br>",
-                         "stars: ", business_city()$stars)
-    #define the color palette for the sentiment
+                         "stars: ", business_city()$stars, "<br>")
+    
+    # define the color palette for the sentiment
     col_pal <- colorFactor(c("red", "blue", "green"), business_city()$sentiment)
     
     #build the map
     bar_locations <- leaflet(business_city()) %>%
       addTiles() %>%
-      addCircleMarkers(lng = ~longitude, lat = ~latitude, popup = description, 
+      addCircleMarkers(lng = ~longitude, lat = ~latitude, popup = description,
                        color = ~col_pal(sentiment), stroke = FALSE, fillOpacity = 0.5,
                        layerId = ~business_id) %>%
       addLegend("bottomleft", pal=col_pal, values=c("Negative", "Neutral", "Positive"), title="Sentiment",
@@ -39,11 +41,6 @@ server <- function(input, output) {
     bar_locations
     
   })
-  
-  
-  # output$plot1 <- renderPlot({
-  #   ggplot(business_city(), aes(x=stars)) + geom_histogram(binwidth = 0.5)
-  # })
   
   ## observing a click will return the `id` you assigned in the `layerId` argument
   observeEvent(input$yelp_marker_click, {
@@ -61,13 +58,40 @@ server <- function(input, output) {
         geom_vline(aes(xintercept=review_city()[business_id == click$id, average_stars]),
                    color="red", linetype="dashed", size=1) +
         geom_text(aes(x=review_city()[business_id == click$id, average_stars]),
-                  label=paste("percentile in ", input$cityname, " : ", percentile), y=0.3, vjust=1.5, hjust=1, size=4)
+                  label=paste(business[business_id==click$id, name], " percentile in ", input$cityname, " : ", percentile), y=0.3, vjust=1.5, hjust=1, size=4) +
+        ggtitle(paste0("Stars distirbution in ", input$cityname )) +
+        theme(plot.title = element_text(size = 20, face = "bold", hjust = 0.5))
       
     })
     
+    # output$plot2 <- renderPlot({
+    #   
+    #   ggplot(review_city(), aes(x=average_stars)) + geom_density() +
+    #     geom_vline(aes(xintercept=review_city()[business_id == click$id, average_stars]),
+    #                color="red", linetype="dashed", size=1) +
+    #     geom_text(aes(x=review_city()[business_id == click$id, average_stars]),
+    #               label=paste("percentile: ", percentile), y=0.3, vjust=1.5, hjust=1, size=4)
+    #   
+    # })
+    
+    output$text1 <- renderTable({
+      x <- as.data.frame(customer_sug_function(click$id, input$cityname))
+      names(x) <- "Suggestion"
+      x
+    })
+    
+    output$text2 <- renderTable({
+      y <- as.data.frame(business_sug_function(click$id, input$cityname))
+      names(y) <- "Suggestion"
+      y
+    })
+    
+    # output$text1 <- renderTable({
+    #   y <- as.data.frame(customer_sug_function(click$id))
+    #   names(y) <- "Suggestion"
+    #   y
+    # })
+    
   })
-  
-  
-  #add distribution
   
 }
